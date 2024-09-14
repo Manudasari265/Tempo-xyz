dfd0987e5110d3ace0b9d7fc395a5824da179238
Manoj Dasari <dasarimanoj265@gmail.com>
2024-09-15 00:00:06 +0530

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>We can get started i guess</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
